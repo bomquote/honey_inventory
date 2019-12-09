1ea09861914d5d80c37552d823f409e53730970a
bmjjr <bob@smewp.net>
2019-12-08 23:07:14 -0800

    implemented initial pass for transfer_location

--- bin/manage.py
+++ bin/manage.py
@@ -61,9 +61,6 @@ def delete_location(label, warehouse):
     """
     Delete a location. If the location contains inventory then offer to transfer
     the inventory to a new or existing location.
-    todo: if the location has inventory associated with it, ask the user if they would
-        like to transfer the inventory to a new or existing location
-    would like to transfer the inventory to a new inventory location.
     """
     if label == 'Required':
         return print("You must provide a location label.")
@@ -120,15 +117,88 @@ def delete_location(label, warehouse):
                         print(
                             f'Transferred inventory to {transfer_location} and deleted {original_location}')
                         return original_location.delete()
-            return print('Cancelling the delete due to bad transfer label')
+            return print('Delete cancelled due to bad transfer label')
         else:
-            return print('cascade delete finished.')
+            return print('Delete cancelled due to unhandled inventory transfer.')
     # InventoryLocation.create(label=label, warehouse_id=wh.id)
     if original_location:
-        print(f"Deleted location.")
+        print(f"Deleted location {original_location}.")
         return original_location.delete()
     return print(f"Location does not exist so nothing was deleted.")
 
+
+@inv.command()
+@click.option("--original_warehouse",
+              prompt="Enter the original warehouse name",
+              default="Garage", help="Select warehouse.")
+@click.option("--original_label", prompt="Enter the original inventory location label",
+              default="Required", help="Provide the location label.")
+def transfer_location(original_warehouse, original_label):
+    """
+    Transfer all of the inventory registered in an original location to a new or
+    existing location.
+    """
+    if original_label == 'Required':
+        return print("You must provide a location label.")
+    wh = session.query(Warehouse).filter_by(name=original_warehouse).first()
+    if not wh:
+        return print(
+            f"The {original_warehouse} warehouse doesn't exist. Please create it first.")
+    original_location = session.query(InventoryLocation).filter(
+        InventoryLocation.label == original_label,
+        InventoryLocation.warehouse_id == wh.id).first()
+    if original_location and original_location.skus:
+        print(f"The following skus exist in the location: "
+                     f"{[(inst.sku.sku, str(inst.quantity)+' pcs') for inst in original_location.skus]}")
+        answer = input('Shall we transfer all of the inventory at the location [Yes]? ') or "Yes"
+        if answer.lower() == 'yes':
+            transfer_warehouse = input('Enter the warehouse name for the transfer [Garage]: ') or 'Garage'
+            transfer_warehouse_obj = session.query(Warehouse).filter_by(name=transfer_warehouse).first()
+            if not transfer_warehouse_obj:
+                return print(
+                    f"The {transfer_warehouse} warehouse doesn't exist. "
+                    f"Please create it first using `create_warehouse`.")
+            def transf():
+                """Use this to give two attempts to enter the label."""
+                return input('Enter the transfer label [Required]: ') or None
+            transfer_label = transf()
+            if not transfer_label:
+                # user error, assign and run it again
+                transfer_label = transf()
+            if transfer_label:
+                # search to see if the warehouse/label location exists.
+                transfer_location = session.query(InventoryLocation).filter(
+                    InventoryLocation.label == transfer_label,
+                    InventoryLocation.warehouse_id == transfer_warehouse_obj.id).first()
+                if transfer_location:
+                    # transfer the inventory from original location to the new location
+                    for inst in original_location.skus:
+                        inst.update(location_id=transfer_location.id)
+                    return print(f'Transferred inventory to {transfer_location}')
+                else:
+                    create_new_label = input(
+                        f"The label {transfer_label} doesn't exist at the {transfer_warehouse_obj.name} warehouse. Create it now and transfer? [Yes]: ") or 'Yes'
+                    if create_new_label == "Yes":
+                        InventoryLocation.create(
+                            label=transfer_label,
+                            warehouse_id=transfer_warehouse_obj.id)
+                        print(f"Created the new inventory location with label {transfer_label}")
+                        transfer_location = session.query(InventoryLocation).filter(
+                            InventoryLocation.label == transfer_label,
+                            InventoryLocation.warehouse_id == transfer_warehouse_obj.id).first()
+                        # transfer from original location to the new location
+                        for inst in original_location.skus:
+                            inst.update(location_id=transfer_location.id)
+                        return print(
+                            f'Transferred inventory to {transfer_location} and deleted {original_location}')
+            return print('Transfer cancelled due to bad transfer label.')
+        else:
+            return print("This method only transfers all the SKU's from one location "
+                         "to another. Use `transfer-skus` to manage individual SKU's.")
+    # InventoryLocation.create(label=label, warehouse_id=wh.id)
+    if original_location:
+        return print(f"Location {original_location} has no inventory to transfer.")
+    return print(f"Location <InventoryLocation ('{original_label}', '{original_warehouse}')> does not exist.")
 
 @inv.command()
 @click.option("--count", default=1, help="Number of repeats.")
